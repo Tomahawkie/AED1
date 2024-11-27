ae8f1553ba972de7a8ed0730559af916f142d995
Caio Veloso <130678404+Tomahawkie@users.noreply.github.com>
2024-11-27 14:36:31 -0300

    main.c

--- Lista encadeada/main.c
+++ Lista encadeada/main.c
@@ -15,7 +15,40 @@ Considere os seguintes requisitos adicionais:
  Implemente uma função para imprimir todos os elementos da lista.*/
 int main(){
     Livro* lista = malloc(sizeof(Livro));
-    int tam = 0;
-    
+    int tam = 0, posicao, opcao;
+    do {
+    printf("1. Para adicionar\n2. Para adicionar em uma posicao especifica\n3. Para remover em uma posicao especifica\n4. para remover o ultimo\n5. Para obter a posicao do item\n 6. Para imprimir a lista\n7. Para sair\n");
+    scanf("%d[^\n]", opcao);
+    switch (opcao)
+    {
+    case 1:
+        lista = push(lista, &tam);
+        break;
+    case 2:
+        printf("Em qual posicao?\n");
+        scanf("%d[^\n]", posicao);
+        push_selection(&lista, &posicao, &tam);
+        break;
+    case 3:
+        printf("Em qual posicao?\n");
+        scanf("%d[^\n]", posicao);
+        remover(&lista, &posicao, &tam);
+    case 4:
+        remover_ultimo(lista, &tam);
+    case 5:
+        printf("Em qual posicao?\n");
+        scanf("%d[^\n]", posicao);
+        search(lista, posicao);
+    case 6:
+        imprimir(lista);
+    case 7:
+        printf("Saindo...");
+        free(lista);
+        exit(EXIT_FAILURE);
+    default:
+        printf("opcao invalida");
+        break;
+    }while(opcao != 7)
+
     return 0;
 }
